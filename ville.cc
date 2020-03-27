5626cedb631da4130c67744b60ddee068b8dc664
Civ0dul <62757060+Civ0dul@users.noreply.github.com>
2020-03-27 16:23:47 +0100

    Update ville.cc

--- ville.cc
+++ ville.cc
@@ -14,17 +14,22 @@ using namespace std;
 
 typedef vector<Noeud> Web;
 
+bool check_link (Noeud noeud1, Noeud noeud2);
+bool check_identical_uid (unsigned int uid);
+bool check_link_vacuum (Links lien);
+bool check_max_link (Noeud noeud);
+bool check_multiple_same_link (Links lien);
 
 class Ville{
 public:
 	Ville(unsigned int nbrL=0, unsigned nbrT=0, unsigned int nbrP=0);
-	void AddNode (Noeud noeud);
-	void AddLink (Liste link);
-	Web GetN () const;
-	vector<Liste> GetLiens () const;
+	void addNode (Noeud noeud);
+	void addLink (Liste link);
+	Web getN () const;
+	vector<Links> getLiens () const;
 	
 private:
-	vector<Liste> liens;
+	vector<Links> liens;
 	Web N;
 	unsigned int nombreL;
 	unsigned int nombreT;
@@ -40,18 +45,56 @@ Ville::Ville(unsigned int nbrL, unsigned nbrT, unsigned int nbrP)
 
 Ville v;
 
-Web Ville::GetN () const {return N;}
-vector<Liste> Ville::GetLiens () const {return liens;}	
+Web Ville::getN () const {return N;}
+vector<Links> Ville::getLiens () const {return liens;}	
 
-void Ville::AddNode(Noeud noeud) {
-	
+void Ville::addNode(Noeud noeud) {
+	if (check_identical_uid(noeud.getUid())) exit(1);
 	N.push_back(noeud);
 }
 
-void Ville::AddLink(Liste link) {
+void Ville::addLink(Links link) {
+	if (check_link_vacuum(link)) exit(1);
+	if (check_multiple_same_link(link)) exit(1);
 	liens.push_back(link);
 }
 
+bool check_identical_uid (unsigned int uid) {
+	for (auto i : v.getN()){
+		if (i.getUid() == uid){
+			cout << error::identical_uid(uid);
+			return true;
+		}
+	}
+	return false;
+}
+
+bool check_link_vacuum (Links lien) {
+	for (auto i : v.getN()){
+		if (lien[1] == i.getUid()){
+			return false;
+		}
+	}
+	cout << error::link_vacuum(lien[1]);
+	return true;
+}
+
+bool check_max_link (Noeud noeud) {
+	if (noeud.getType() == Logement and noeud.getV().size() >= max_link)
+	{ 
+		return false; 
+	}
+	return true;
+}
+
+bool check_multiple_same_link (Links lien) {
+	for (auto i : v.getLink()){
+		if (lien[0] == i[1] and lien[1] == i[0]){
+			cout << error::multiple_same_link(i[0], i[1]);
+			return true;
+				}
+	return false;
+}
 
 //-----------Vérifications---------------
 
@@ -59,20 +102,20 @@ bool check_node_node_superposition(Noeud noeud) const {
 	bool resultat(true);
 	double x1,y1,x2,y2,r1,r2,dx,dy;
 	double distance;
-	x1 = noeud.GetCentre().GetX();
-	y1 = noeud.GetCentre().GetY();
-	r1 = sqrt(noeud.GetCapacity());
+	x1 = noeud.getCentre().GetX();
+	y1 = noeud.getCentre().GetY();
+	r1 = sqrt(noeud.getCapacity());
 	
-	for(auto& element : v.GetN()) {
-		x2 = element.GetCentre().GetX();
-		y2 = element.GetCentre().GetY();
-		r2 = sqrt(element.GetCapacity());
+	for(auto& element : v.getN()) {
+		x2 = element.getCentre().getX();
+		y2 = element.getCentre().getY();
+		r2 = sqrt(element.getCapacity());
 		dx = x1-x2;
 		dy = y1-y2;
 		distance = sqrt(dx*dx + dy*dy);
 		if(distance <= r1+r2) {
 			resultat = false;
-			cout << error::node_node_superposition(noeud.GetUid(), element.GetUid());
+			cout << error::node_node_superposition(noeud.getUid(), element.getUid());
 		}
 	}
 	return resultat;
@@ -144,7 +187,7 @@ void decodage_ligne(string line){
 
 	case HOUSING: 
 		type = Logement;
-		v.AddNode(analyse_ligne_noeud(line, type));
+		v.addNode(analyse_ligne_noeud(line, type));
 		++i;
 		if(i == total) etat=NB1 ;
 	    break;
@@ -157,7 +200,7 @@ void decodage_ligne(string line){
 
 	case TRANSPORT:
 		type = Transport;
-		v.AddNode(analyse_ligne_noeud(line, type));
+		v.addNode(analyse_ligne_noeud(line, type));
 		++i;
 		if(i == total) etat=NB2 ;
 	    break;
@@ -170,7 +213,7 @@ void decodage_ligne(string line){
 
 	case PRODUCTION: 
 		type = Production;
-		v.AddNode(analyse_ligne_noeud(line, type));
+		v.addNode(analyse_ligne_noeud(line, type));
 		++i;
 		if(i == total) etat=NB3 ;
 	    break;
@@ -182,7 +225,7 @@ void decodage_ligne(string line){
 		break;
 
 	case LINK:
-		v.AddLink(analyse_ligne_link(line));
+		v.addLink(analyse_ligne_link(line));
 		++i;
 		if(i == total) etat=FIN ;
 	    break;
